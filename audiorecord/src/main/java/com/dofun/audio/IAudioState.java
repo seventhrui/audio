fd780fb3c85664e43a93728266e92d50e8f7c30c
gaorui <rui.gao2011@gmail.com>
2023-06-28 11:38:14 +0800

    v0.0.1

---
+++ audiorecord/src/main/java/com/dofun/audio/IAudioState.java
@@ -0,0 +1,11 @@
+package com.dofun.audio;
+
+public abstract class IAudioState {
+    public IAudioState() {
+    }
+
+    void enter() {
+    }
+
+    abstract void handleMessage(AudioStateMessage var1);
+}
